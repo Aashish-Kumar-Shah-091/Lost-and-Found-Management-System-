131d42954a9c226e4c688bde20653cdc0a81ee60
Aashish012-rgb <182605557+Aashish012-rgb@users.noreply.github.com>
2026-05-31 21:05:26 +0545

    fixed the image error

--- lostFound/settings.py
+++ lostFound/settings.py
@@ -150,9 +150,9 @@ import cloudinary.uploader
 import cloudinary.api
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME', ''),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY', ''),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET', ''),
+    'CLOUD_NAME': os.environ.get('Root', ''),
+    'API_KEY': os.environ.get('642252122754475', ''),
+    'API_SECRET': os.environ.get('gMImWB_gcj9RGFPFq3VuqJWO-io', ''),
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
